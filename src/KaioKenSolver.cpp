1ba65b80b46d68cefd7f11420a10997c196f6ca7
Franco Frizzo <francofrizzo@gmail.com>
2016-03-22 01:01:42 -0300

    Fixed edge case N=1 in KaioKen and minor bugs + comments and cosmetic changes in code

--- src/KaioKenSolver.cpp
+++ src/KaioKenSolver.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
-#include <algorithm>  
+#include <algorithm>
 #include <vector>
 
 using namespace std;
-//Numero de Peleadores
 
+/*
+**  Funciones auxiliares
+*/
 
-void printVec(vector < int > vec){
-     for (int i = 0; i < vec.size(); ++i){
-            cout << vec[i] << " "  ;
-        }
-        cout << endl ;
+// Imprime un vector (de enteros) a la salida estándar.
+// Los elementos se imprimen separados por espacios,
+// en una única línea seguida de un caracter de fin de línea.
+void printVec(vector < int > vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
 }
 
-void printVecs(vector < vector < int > > vecs){
+// Recibe una lista de vectores. Imprime a la salida estándar
+// el número de elementos de la lista, y a continuación todos
+// los vectores, cada uno en una nueva línea
+void printVecs(vector < vector < int > > vecs) {
     cout << vecs.size() << endl;
-    for (int i = 0; i < vecs.size(); ++i){
+    for (int i = 0; i < vecs.size(); ++i) {
         printVec(vecs[i]);
-       
     }
 }
 
-
-
-vector < vector < int > >  merge ( vector < vector < int > > peleasIzq , vector < vector < int > > peleasDer){
+// Recibe dos listas de vectores y concatena cada vector de la primera
+// lista con el respectivo de la segunda lista. Si una de las listas es
+// más larga que la otra, omite los elementos sobrantes.
+vector < vector < int > >  merge (vector < vector < int > > peleasIzq, vector < vector < int > > peleasDer) {
     vector < vector < int > > peleas;
-    int minSize =  min(peleasIzq.size(),peleasDer.size());
-    for (int i = 0; i < minSize; ++i){
+    int minSize =  min(peleasIzq.size(), peleasDer.size());
+    for (int i = 0; i < minSize; ++i) {
         vector < int > peleaIzq  = peleasIzq[i];
         vector < int > peleaDer  = peleasDer[i];
         peleaIzq.insert(peleaIzq.end(), peleaDer.begin(), peleaDer.end());
@@ -35,61 +43,78 @@ vector < vector < int > >  merge ( vector < vector < int > > peleasIzq , vector 
     return peleas;
 }
 
-vector < vector < int > >  generarPeleas( int N ){
-    
+/*
+**  Resolución del ejercicio
+*/
+
+// Resuelve la instancia del problema correspondiente a un ejército de N guerreros.
+vector < vector < int > > generarPeleas(int N) {
+
     vector < vector < int > > peleas;
-    
-    // Casos Base
-    if(N == 1){
-        vector < int > pelea ;
+
+    // Casos base
+    if (N == 1) {
+        return peleas;
+    }
+
+    if (N == 2) {
+        vector < int > pelea;
         pelea.push_back(1);
-
-        peleas.push_back(pelea);
-        return  peleas;
-    }
-    if (N == 2){
-
-        vector < int > pelea ;
-        pelea.push_back(1);;
         pelea.push_back(2);
-        
+
         peleas.push_back(pelea);
-        return  peleas;
+        return peleas;
     }
-    if (N%2 == 0){
-        peleas = merge(generarPeleas( N/2 ),generarPeleas( N/2));
-    }else{
-        vector < vector < int > > peleasIzq = generarPeleas( (N/2) );
-        vector < vector < int > > peleasDer = generarPeleas( (N/2) + 1);  
-        peleas = merge( peleasIzq,peleasDer);
-        if(peleasIzq.size() != peleasDer.size()){
-             vector < int > lastPeleaIzq  = peleasIzq[peleasIzq.size() -1];
-            vector < int > lastPeleaDer  = peleasDer[peleasDer.size() -1];
-            lastPeleaDer.insert(lastPeleaDer.end(), lastPeleaIzq.begin(), lastPeleaIzq.end());
-            peleas.push_back(lastPeleaDer);
-        }
-       
-        
-    }
-    
+
+    // Caso recursivo
+
+    // Primer pelea
     vector < int > pelea ;
     int i;
-    for (i = 0; i < N/2; ++i){
+    for (i = 0; i < N / 2; ++i) {
         pelea.push_back(1);
     }
-    for (i; i < N; ++i){
+    for (i; i < N; ++i) {
         pelea.push_back(2);
     }
     peleas.push_back(pelea);
+
+    // Llamadas recursivas para completar las peleas faltantes
+    if (N % 2 == 0) {
+        // Si N es un número par, las sub-soluciones tienen igual tamaño.
+        vector < vector < int > > peleasRecurs = merge(generarPeleas(N / 2), generarPeleas(N / 2));
+        peleas.insert(peleas.end(), peleasRecurs.begin(), peleasRecurs.end());
+    } else {
+        vector < vector < int > > peleasIzq = generarPeleas(N / 2);
+        vector < vector < int > > peleasDer = generarPeleas((N / 2) + 1);
+        vector < vector < int > > peleasRecurs = merge(peleasIzq, peleasDer);
+        peleas.insert(peleas.end(), peleasRecurs.begin(), peleasRecurs.end());
+
+        // Si N es impar, las sub-soluciones podrían tener tamaños diferentes.
+        // En tal caso, la sub-solución izquierda tendrá un elemento menos que la derecha,
+        // por lo que es completada replicando su última pelea para así poder
+        // concatenarla con la última pelea de la sub-solución derecha.
+        if (peleasIzq.size() != peleasDer.size()) {
+            vector < int > lastPeleaIzq;
+            vector < int > lastPeleaDer;
+
+            if (peleasIzq.size() > 0) {
+                lastPeleaIzq = peleasIzq[peleasIzq.size() - 1];
+            } else {
+                lastPeleaIzq.push_back(1);
+            }
+            lastPeleaDer = peleasDer[peleasDer.size() - 1];
+            
+            lastPeleaIzq.insert(lastPeleaIzq.end(), lastPeleaDer.begin(), lastPeleaDer.end());
+            peleas.push_back(lastPeleaIzq);
+        }
+    }
+
     return peleas;
 }
 
-int main(){
-    int N ;
-    cin >> N ;
+int main() {
+    int N;
+    cin >> N;
     printVecs(generarPeleas(N));
-    
-    
 }
-
-
